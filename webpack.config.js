93138729b184adf437b3e95e77aac12ac3d43be0
Jarold Wong <jaroldw@gmail.com>
2017-06-12 15:17:49 -0700

    housekeeping

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './client/app.js',
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'index_bundle.js',
+    filename: 'bundle.js',
     publicPath: '/'
   },
   module: {
